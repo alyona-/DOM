d5a4eb46f74050f59e5b0a2653f64265214cc1fc
alyona- <alyona_om@mail.ru>
2014-02-05 17:26:36 +0200

    Change js

--- homework.js
+++ homework.js
@@ -1,60 +1,68 @@
 /**
-* Обработчик клика по ссылке с классом 'popup-link'
-* @param {Event} e событие клика
-* @private
-*/
+ * Обработчик клика по ссылке с классом 'popup-link'
+ * @param {Event} e событие клика
+ * @private
+ */
 
 
-var link = document.getElementsByClassName('popup-link');
-var linkArr = new Array();
 
-for(i=0; i<link.length; i++) {
-    linkArr[i] = link[i].addEventListener('click', function () {
-    if(this.preventDefault) {
-        this.preventDefault();
-        openPopupFromLink(this);
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(event) {
+    var target = event.target;
+    while (target.tagName !== 'LI') {
+        target = target.parentNode;
+    }
+    var link= target.children[0];
+    if(event.preventDefault) {
+        event.preventDefault();
+        openPopupFromLink(link);
     }
     else{
         event.returnValue = false;
-        openPopupFromLink(this);
-        }
-    }, false);
-}
+        openPopupFromLink(link);
+    }
+});
 
 /**
-* Получает данные из ссылки
-* на основе этих данных создаёт попап (через createPopup) и добавляет его в DOM
-* @param {HTMLElement} link Ссылка с data-аттрибутами
-*/
+ * Получает данные из ссылки
+ * на основе этих данных создаёт попап (через createPopup) и добавляет его в DOM
+ * @param {HTMLElement} link Ссылка с data-аттрибутами
+ */
 
 function openPopupFromLink(link) {
-    createPopup(
-        link.getAttribute('data-title'),
-        link.getAttribute('data-message').replace('%s' , link.getAttribute('href')),
-        function onOk (){
-            location.href = link.getAttribute('href');;
-        }
-    );
+    var title = link.getAttribute('data-title');
+    var message = link.getAttribute('data-message').replace('%s' , link);
+    createPopup(title, message,function onOk (){
+        location.href = link;
+    });
 }
 
 /**
-* Создаёт DOM-узел с сообщением
-* @param {String} title Заголовок сообщение
-* @param {String} message Текст сообщения сообщение
-* @param {Function} onOk Обработчик клика по кнопке 'Да'
-* @returns {HTMLElement}
-*/
+ * Создаёт DOM-узел с сообщением
+ * @param {String} title Заголовок сообщение
+ * @param {String} message Текст сообщения сообщение
+ * @param {Function} onOk Обработчик клика по кнопке 'Да'
+ * @returns {HTMLElement}
+ */
 
 function createPopup(title, message, onOk) {
     var parentEl = document.body;
     var winda = document.createElement('div');
     var  str ='<div class="fon"></div><div class="window"><h4>'+title+'</h4><p>'+message+'</p>' +
-                '<input type="button" value="Да" class="buttons" id="button_1">' +
-                '<input type="button" value="Нет" class="buttons" id="button_2"></div>';
+        '<input type="button" value="Да"  value="1" class="buttons" id="button_1">' +
+        '<input type="button" value="Нет" class="buttons" id="button_2"></div>';
     parentEl.appendChild(winda);
     winda.innerHTML = str;
-    var input1 = document.getElementById('button_1').addEventListener('click',onOk, false);
-    var input2 = document.getElementById('button_2').addEventListener('click',function (){
-    parentEl.removeChild(winda);
-    }, false);
+    winda.addEventListener('click', function(event) {
+        var target = event.target;
+      while (target.tagName == 'INPUT') {
+            target = target.parentNode;
+
+        }
+        if(event.target.value === 'Да') {
+            onOk();
+        }else if(event.target.value === 'Нет'){
+            parentEl.removeChild(winda);
+        }  }); 
 }

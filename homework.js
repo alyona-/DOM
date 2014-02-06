5993ced208286db365740eebe39be978107b0c51
alyona- <alyona_om@mail.ru>
2014-02-06 15:29:28 +0200

    Change

--- homework.js
+++ homework.js
@@ -4,25 +4,36 @@
  * @private
  */
 
+window.onload  = function (){
+    win = false;
+}
 
-
-
-var list = document.querySelector('ul');
+var list = document.querySelector('body');
 list.addEventListener('click', function(event) {
+    var link;
     var target = event.target;
-    while (target.tagName !== 'LI') {
-        target = target.parentNode;
+    if(target.tagName == 'A' && target.className == 'popup-link')  {
+        link = target; next();
+    } else  if(target.parentNode.tagName == 'A' && target.parentNode.className == 'popup-link')  {
+        link = target.parentNode;
+        next();
+    } else if(target.childNodes.tagName == 'A' && target.childNodes.className == 'popup-link') {
+        link = target.children[0];
+        next();
     }
-    var link= target.children[0];
-    if(event.preventDefault) {
-        event.preventDefault();
-        openPopupFromLink(link);
-    }
-    else{
-        event.returnValue = false;
-        openPopupFromLink(link);
+    function next(){
+        if(event.preventDefault) {
+            event.preventDefault();
+            openPopupFromLink(link);
+        }
+        else{
+            event.returnValue = false;
+            openPopupFromLink(link);
+        }
     }
 });
+
+
 
 /**
  * Получает данные из ссылки
@@ -31,11 +42,11 @@ list.addEventListener('click', function(event) {
  */
 
 function openPopupFromLink(link) {
-    var title = link.getAttribute('data-title');
-    var message = link.getAttribute('data-message').replace('%s' , link);
-    createPopup(title, message,function onOk (){
-        location.href = link;
-    });
+    createPopup(link.getAttribute('data-title'),
+       link.getAttribute('data-message').replace('%s' , link),
+        function onOk (){
+            location.href = link.getAttribute('href');
+        });
 }
 
 /**
@@ -47,22 +58,19 @@ function openPopupFromLink(link) {
  */
 
 function createPopup(title, message, onOk) {
-    var parentEl = document.body;
-    var winda = document.createElement('div');
+    parentEl = document.body;
+    (win == false)?winda = document.createElement('div'):winda.style.display = 'block';
     var  str ='<div class="fon"></div><div class="window"><h4>'+title+'</h4><p>'+message+'</p>' +
         '<input type="button" value="Да"  value="1" class="buttons" id="button_1">' +
         '<input type="button" value="Нет" class="buttons" id="button_2"></div>';
     parentEl.appendChild(winda);
     winda.innerHTML = str;
     winda.addEventListener('click', function(event) {
-        var target = event.target;
-      while (target.tagName == 'INPUT') {
-            target = target.parentNode;
-
-        }
+	  var target = event.target.parentNode;
         if(event.target.value === 'Да') {
             onOk();
         }else if(event.target.value === 'Нет'){
-            parentEl.removeChild(winda);
-        }  }); 
+			winda.style.display = 'none';
+        }  });
+        win = true;
 }
